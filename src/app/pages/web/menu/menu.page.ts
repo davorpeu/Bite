968edc43ba07427e7b7cb61b77b37c681a186b19
dadop <davorpeu@gmail.com>
2021-08-11 16:13:40 +0200

    menu

--- src/app/pages/web/menu/menu.page.ts
+++ src/app/pages/web/menu/menu.page.ts
@@ -12,4 +12,23 @@ export class MenuPage implements OnInit {
   ngOnInit() {
   }
 
+  polje = [
+    {
+      name: 'prvo jelo',
+      id:1,
+      img: './assets/Meal/meat.png'
+    },
+    {
+      name: 'drugo jelo',
+      id:2,
+      img: './assets/Meal/meat.png'
+    },
+    {
+      name: 'drugo jelo',
+      id: 3,
+      img: './assets/Meal/meat.png'
+    }
+  ]
+
 }
+
