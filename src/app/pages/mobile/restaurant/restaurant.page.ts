5da3c6a9d3dfa0f0595ee63f075a34e8428026b7
dadop <davorpeu@gmail.com>
2021-08-18 16:27:09 +0200

    mobile menu

--- src/app/pages/mobile/restaurant/restaurant.page.ts
+++ src/app/pages/mobile/restaurant/restaurant.page.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Restaurant } from 'src/app/interfaces/mobile/restaurant';
+import { MenuDish, Restaurant } from 'src/app/interfaces/mobile/restaurant';
 import { RestaurantService } from 'src/app/services/restaurant/restaurant.service';
 
 @Component({
@@ -9,6 +9,7 @@ import { RestaurantService } from 'src/app/services/restaurant/restaurant.servic
   styleUrls: ['./restaurant.page.scss'],
 })
 export class RestaurantPage implements OnInit {
+
 
   constructor(private route: ActivatedRoute,
     private restaurantService: RestaurantService
@@ -20,27 +21,31 @@ export class RestaurantPage implements OnInit {
     // this.route.queryParams.subscribe(params => {
     //   console.log(params)
     // })
-this.selectedRestaurant = this.restaurantService.selectedRestaurant
+    this.selectedRestaurant = this.restaurantService.selectedRestaurant
 
-this.restaurantService.getWeekMenuForSelectedRestaurant();
+    this.restaurantService.getWeekMenuForSelectedRestaurant();
   }
 
   currentDay = 1;
+
   days = [1, 2, 3, 4, 5];
+
   daysName = ['MON', 'TUE', 'WEN', 'THU', 'FRI'];
+  
   daysNamesCro = ['Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak']
+
+  filteredRestaurantMenus: MenuDish[][] = []
 
   changeDay(day: number) {
     this.currentDay = day;
     this.restaurantService.onDayChanged(day);
+
     this.getMenusForDay();
   }
 
   getMenusForDay() {
-    // if (this.fetchedMenus != null) {
-    //   this.filteredFetchedMenus = this.fetchedMenus.filter(o => o.day == this.currentDay);
-    // }
-    console.log("menu");
+    if (this.selectedRestaurant != null) {
+      this.filteredRestaurantMenus = this.selectedRestaurant.menu.filter(o => o.day == this.currentDay);
+    }
   }
-
 }
