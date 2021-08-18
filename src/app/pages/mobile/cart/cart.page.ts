702e0fe44b90b519a380c5ee3eb43316eaa2dd67
davorpeu <davorpeu@gmail.com>
2021-08-18 19:54:34 +0200

    toast added

--- src/app/pages/mobile/cart/cart.page.ts
+++ src/app/pages/mobile/cart/cart.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { MenuDish } from 'src/app/interfaces/mobile/restaurant';
 import { CartService } from 'src/app/services/cart/cart.service';
 
@@ -10,14 +11,27 @@ import { CartService } from 'src/app/services/cart/cart.service';
 export class CartPage implements OnInit {
   orders: MenuDish[];
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService,
+    private toastController: ToastController) { }
 
   ngOnInit() {
     // sort od manjeg do većeg dana od 0 do 4
-    this.cartService.orders.subscribe(orders => this.orders = orders.sort((a,b) => a.day - b.day));
+    this.cartService.orders.subscribe(orders => this.orders = orders.sort((a, b) => a.day - b.day));
   }
 
-  async finishOrder(){
+  async finishOrder() {
     await this.cartService.finishOrder();
+    this.presentToast();
+    //dodaj button finishOrder
   }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Order confirmed',
+      duration: 2000,
+      color: 'primary'
+    })
+    toast.present();
+  }
+
 }
